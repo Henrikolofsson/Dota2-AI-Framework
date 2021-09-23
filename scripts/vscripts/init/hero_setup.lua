e385c0bfbf66be007e1dd57e1e07307e05288651
olof <olof@takman.se>
2021-09-23 23:44:37 +0200

    Extracted callback functions for hero selection.

--- scripts/vscripts/init/hero_setup.lua
+++ scripts/vscripts/init/hero_setup.lua
@@ -4,20 +4,27 @@ local HeroSetup = {}
 
 local selected_heroes = {}
 
+function HeroSetup:Pick_radiant_team_heroes()
+    print("Selecting radiant heroes after delay")
+    for player_id = 0, 4 do
+        PlayerResource:SetCustomTeamAssignment(player_id, DOTA_TEAM_GOODGUYS)
+        PlayerResource:GetPlayer(player_id):SetSelectedHero(HeroSetup:Get_random_hero())
+        if player_id ~= 0 then
+            SendToServerConsole("kickid " .. tostring(player_id + 1))
+        end
+    end
+end
+
 function HeroSetup:Select_radiant_heroes()
     print("Selecting radiant heroes")
-    Timers:CreateTimer(
-        function()
-            print("Selecting radiant heroes after delay")
-            for player_id = 0, 4 do
-                PlayerResource:SetCustomTeamAssignment(player_id, DOTA_TEAM_GOODGUYS)
-                PlayerResource:GetPlayer(player_id):SetSelectedHero(HeroSetup:Get_random_hero())
-                if player_id ~= 0 then
-                    SendToServerConsole("kickid " .. tostring(player_id + 1))
-                end
-            end
-        end
-    )
+    Timers:CreateTimer(HeroSetup.Pick_radiant_team_heroes)
+end
+
+function HeroSetup:Set_dire_team_bots()
+    local lanes = {"mid", "top", "bot", "top", "bot"}
+    for i = 1, 5 do
+        Tutorial:AddBot(HeroSetup:Get_random_hero(), lanes[i], "easy", false)
+    end
 end
 
 function HeroSetup:Select_dire_heroes()
@@ -28,11 +35,7 @@ function HeroSetup:Select_dire_heroes()
             if not Game_states:Is_game_in_progress_state() then
                 return 1.0
             end
-
-            local lanes = {"mid", "top", "bot", "top", "bot"}
-            for i = 1, 5 do
-                Tutorial:AddBot(HeroSetup:Get_random_hero(), lanes[i], "easy", false)
-            end
+            HeroSetup:Set_dire_team_bots()
         end
     )
 end
