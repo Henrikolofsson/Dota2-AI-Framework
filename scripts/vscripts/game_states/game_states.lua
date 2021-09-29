bec462bc5e850b305e9c1871541ba840b1c7e87a
olof <olof@takman.se>
2021-09-29 10:58:10 +0200

    Added "Type hint" comments.

--- scripts/vscripts/game_states/game_states.lua
+++ scripts/vscripts/game_states/game_states.lua
@@ -1,13 +1,16 @@
 local Game_states = {}
 
+---@return boolean
 function Game_states:Is_hero_selection_state()
     return GameRules:State_Get() == DOTA_GAMERULES_STATE_HERO_SELECTION
 end
 
+---@return boolean
 function Game_states:Is_pre_game_state()
     return GameRules:State_Get() == DOTA_GAMERULES_STATE_PRE_GAME
 end
 
+---@return boolean
 function Game_states:Is_game_in_progress_state()
     return GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS
 end
