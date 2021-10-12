5cf81e4a33219899a0c0f1d478e7a31150755eb7
olof <olof@takman.se>
2021-10-12 15:03:30 +0200

    Testing filtering visible units and buildings.

--- scripts/vscripts/python_AI/python_AI_thinking.lua
+++ scripts/vscripts/python_AI/python_AI_thinking.lua
@@ -1,7 +1,7 @@
 -- Python_AI_thinking
 local Python_AI_thinking = {}
 
-
+local printed = false
 
 ---@param hero table
 ---@return number
@@ -17,6 +17,59 @@ function Python_AI_thinking:OnThink(hero)
         --         allEntities.push(entity)
         -- end
     -- anropa python api: "/update" med allEntities
+
+    if not printed and hero:GetTeam() == DOTA_TEAM_BADGUYS then
+        printed = true
+        -- local towers = Entities:FindAllByClassname("npc_dota_tower")
+        -- for _index, tower in ipairs(towers) do
+        --     if tower:GetName()
+        -- end
+        Timers:CreateTimer({
+            endTime = 40.0,
+            callback = function()
+                local all_units_vulnerable = FindUnitsInRadius(
+                    hero:GetTeamNumber(),
+                    hero:GetOrigin(),
+                    nil,
+                    FIND_UNITS_EVERYWHERE,
+                    DOTA_UNIT_TARGET_TEAM_BOTH,
+                    DOTA_UNIT_TARGET_ALL,
+                    DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE,
+                    FIND_ANY_ORDER,
+                    true
+                )
+
+                local all_units_invulnerable = FindUnitsInRadius(
+                    hero:GetTeamNumber(),
+                    hero:GetOrigin(),
+                    nil,
+                    FIND_UNITS_EVERYWHERE,
+                    DOTA_UNIT_TARGET_TEAM_BOTH,
+                    DOTA_UNIT_TARGET_ALL,
+                    DOTA_UNIT_TARGET_FLAG_INVULNERABLE +
+                    DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE,
+                    FIND_ANY_ORDER,
+                    true
+                )
+
+                -- local visible_units = {}
+                -- for _index, unit in ipairs(all_units) do
+                --     if hero:CanEntityBeSeenByMyTeam(unit) then
+                --         table.insert(visible_units, unit)
+                --     end
+                -- end
+
+                print("Can be seen by" .. hero:GetName() .. ":")
+                for _index, visible_unit in ipairs(all_units_vulnerable) do
+                    print("--- " .. visible_unit:GetName())
+                end
+                for _index, visible_unit in ipairs(all_units_invulnerable) do
+                    print("--- " .. visible_unit:GetName())
+                end
+            end
+        })
+    end
+
     return 0.33
 end
 
