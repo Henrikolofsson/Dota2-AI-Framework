cb952ad384fa7959f3330da3566a936b2df0ca23
Fredrik Jeppsson <fredrik.m.jeppsson@gmail.com>
2021-10-15 12:18:26 +0200

    added server states to webserver.py

--- Server/src/webserver.py
+++ Server/src/webserver.py
@@ -1,24 +1,37 @@
 import json
+from enum import Enum, auto
 
 from bottle import request, response, Bottle
+
+
+class ServerState(Enum):
+    SETTINGS = auto()
+    UPDATE = auto()
 
 
 def setup_web_server(settings_filename, radiant_bot_framework, dire_bot_framework) -> Bottle:
     """Defines the web server routes and return the Bottle app instance."""
     app = Bottle()
-
-    @app.get("/api/party")
-    def party():
-        return json.dumps(radiant_bot_framework.get_party())
-
-    @app.get("/api/enemy_party")
-    def party():
-        return json.dumps(dire_bot_framework.get_party())
+    state = ServerState.SETTINGS
 
     @app.get("/api/settings")
     def settings():
+        nonlocal state
+
+        if state != ServerState.SETTINGS:
+            response.status = 406
+            return {'status:': 'error', 'message': 'invalid server state in settings'}
+
+        r_party = radiant_bot_framework.get_party()
+        d_party = dire_bot_framework.get_party()
         with open(settings_filename) as f:
-            return f.read()
+            user_settings = json.loads(f.read())
+
+        user_settings['radiant_party_names'] = r_party
+        user_settings['dire_party_names'] = d_party
+
+        state = ServerState.UPDATE
+        return json.dumps(user_settings)
 
     @app.post("/api/game_ended")
     def game_ended():
@@ -26,20 +39,24 @@ def setup_web_server(settings_filename, radiant_bot_framework, dire_bot_framewor
 
     @app.post("/api/update")
     def update():
-        return update_game_state(radiant_bot_framework)
+        return update_game_state(radiant_bot_framework, state)
 
     @app.post("/api/radiant_update")
     def radiant_update():
-        return update_game_state(radiant_bot_framework)
+        return update_game_state(radiant_bot_framework, state)
 
     @app.post("/api/dire_update")
     def dire_update():
-        return update_game_state(dire_bot_framework)
+        return update_game_state(dire_bot_framework, state)
 
     return app
 
 
-def update_game_state(bot_framework):
+def update_game_state(bot_framework, state):
+    if state != ServerState.UPDATE:
+        response.status = 406
+        return {'status:': 'error', 'message': 'invalid server state in update game state'}
+
     if request.content_type != 'application/json':
         response.status = 415
         return {'status': 'error', 'message': 'This API only understands JSON'}
