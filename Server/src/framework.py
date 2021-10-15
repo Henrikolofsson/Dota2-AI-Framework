dd94a1c691390fb00df57ba1936db5db04f5a017
Fredrik Jeppsson <fredrik.m.jeppsson@gmail.com>
2021-10-15 13:55:10 +0200

    moved utility functions to util

--- Server/src/framework.py
+++ Server/src/framework.py
@@ -1,10 +1,7 @@
-import importlib
 import json
-import sys
-
-from typing import Any
 
 from bot_framework import BotFramework
+from framework_util import exit_with_error, load_class
 from webserver import setup_web_server
 from pathlib import Path
 
@@ -13,24 +10,7 @@ DIRE_TEAM = 3
 settings_directory = Path(__file__).parent.parent
 settings_filename = settings_directory / 'settings.json'
 
-def load_class(path: str, filename: str, class_name: str) -> Any:
-    """Loads a class, 'class_name' from 'path/filename'. The function
-    returns the class as a value, not an instance of that class."""
-    # It's intuitive to write path using slashes but import_module expects dots.
-    dir_with_bots = path.replace('/', '.')
-    filename_without_py = filename[:-3] if filename[-3:] == '.py' else filename
-    module = importlib.import_module(dir_with_bots + filename_without_py)
-    bot_cls = getattr(module, class_name)
-    return bot_cls
-
-
-def exit_with_error(message: str) -> None:
-    print(message, file=sys.stderr)
-    sys.exit(1)
-
-
 if __name__ == '__main__':
-  
     try:
         with open(settings_filename) as f:
             settings = json.loads(f.read())
@@ -58,7 +38,3 @@ if __name__ == '__main__':
         exit_with_error(f"Couldn't open required key from {settings_filename}: {key_error}")
     except ModuleNotFoundError as module_error:
         exit_with_error(f"Couldn't find the module with the bot class: {module_error}")
-    except AttributeError as attr_error:
-        exit_with_error(f"You probably gave the bot class a different name than the file "
-                        f"that it's in: {attr_error}")
-
---
+++ Server/src/framework_util.py
@@ -0,0 +1,20 @@
+import importlib
+import sys
+
+from typing import Any
+
+
+def load_class(path: str, filename: str, class_name: str) -> Any:
+    """Loads a class, 'class_name' from 'path/filename'. The function
+    returns the class as a value, not an instance of that class."""
+    # It's intuitive to write path using slashes but import_module expects dots.
+    dir_with_bots = path.replace('/', '.')
+    filename_without_py = filename[:-3] if filename[-3:] == '.py' else filename
+    module = importlib.import_module(dir_with_bots + filename_without_py)
+    bot_cls = getattr(module, class_name)
+    return bot_cls
+
+
+def exit_with_error(message: str) -> None:
+    print(message, file=sys.stderr)
+    sys.exit(1)
