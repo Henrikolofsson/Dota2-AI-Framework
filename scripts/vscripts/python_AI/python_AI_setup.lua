86374df21f64a684f88620223da1b908267dc201
Fredrik Jeppsson <fredrik.m.jeppsson@gmail.com>
2021-11-17 16:07:14 +0100

    added a statistics route on server and created a timer that collects and sends statistics periodically on the lua side.

--- scripts/vscripts/python_AI/python_AI_setup.lua
+++ scripts/vscripts/python_AI/python_AI_setup.lua
@@ -25,12 +25,34 @@ end
 ---@param radiant_heroes table
 ---@param dire_heroes table
 function Python_AI_setup:Initialize_bot_thinking(radiant_heroes, dire_heroes)
+    self:Set_statistics_collection(radiant_heroes, dire_heroes)
+
     self:Set_context_think_for_heroes(radiant_heroes)
     if not Settings.should_dire_be_native_bots then
         self:Set_context_think_for_heroes(dire_heroes)
     end
 end
 
+---@param radiant_heroes table
+---@param dire_heroes table
+function Python_AI_setup:Set_statistics_collection(radiant_heroes, dire_heroes)
+    --[[
+        Creates a timer that runs the Python_AI_thinking:Collect_and_send_statistics
+        function once every @collection_interval seconds.
+    ]]
+    local collection_interval = 5
+
+    Timers:CreateTimer(
+        "CollectStatistics",
+        {
+            ---@return number
+            callback = function()
+                Python_AI_thinking:Collect_and_send_statistics(radiant_heroes, dire_heroes)
+                return collection_interval
+            end
+        }
+    )
+end
 
 
 return Python_AI_setup
